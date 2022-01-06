f40af3f6857e9732b3dea556418c2f94abc5dff7
Anurag Mishra <manurag02@gmail.com>
2022-01-06 20:54:22 +0100

    Initial boiler plate

---
+++ src/main/java/com/haufe/beer/beercatalouge/BeercatalougeApplication.java
@@ -0,0 +1,13 @@
+package com.haufe.beer.beercatalouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeercatalougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeercatalougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haufe/beer/beercatalouge/BeercatalougeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haufe.beer.beercatalouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeercatalougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
